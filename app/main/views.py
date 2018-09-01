5933f271685268b30cee2ab22a261ef5736b7c81
smirnov-am <msc.smirnov.am@gmail.com>
2018-09-01 14:07:52 +0200

    add even more headers for frontend

--- app/main/views.py
+++ app/main/views.py
@@ -16,6 +16,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Request-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONSk')
     return response
 
